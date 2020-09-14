4fafcc580dcaa822939936d221ff99ff383d9e3e
Fabian Volkers <fabian.volkers@gmail.com>
2020-09-14 11:29:44 +0200

    autopep8 formatted

--- api/views.py
+++ api/views.py
@@ -1,13 +1,16 @@
-from django.shortcuts import render
-from django.core.exceptions import *
-from .models import Politician, Vote, Poll, PollResult
-from .serializers import PoliticianSerializer, VoteSerializer, PollSerializer, ResultSerializer
-from rest_framework import viewsets, status
-from rest_framework.decorators import action
-from rest_framework.response import Response
-from rest_framework.renderers import JSONRenderer
 import requests
 from django.conf import settings
+from django.core.exceptions import *
+from django.shortcuts import render
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
+from rest_framework.renderers import JSONRenderer
+from rest_framework.response import Response
+
+from .models import Politician, Poll, PollResult, Vote
+from .serializers import (PoliticianSerializer, PollSerializer,
+                          ResultSerializer, VoteSerializer)
+
 
 class PoliticianViewSet(viewsets.ModelViewSet):
     queryset = Politician.objects.all()
@@ -18,7 +21,8 @@ class PoliticianViewSet(viewsets.ModelViewSet):
         votes = Vote.objects.filter(politician=pk)
         vote_list = [vote for vote in votes.values()]
         print(vote_list)
-        serializer = VoteSerializer(data=vote_list, many=True, context={'politician': pk})
+        serializer = VoteSerializer(
+            data=vote_list, many=True, context={'politician': pk})
         print(votes.values_list())
         print(serializer.is_valid())
         if serializer.is_valid():
@@ -29,6 +33,7 @@ class PoliticianViewSet(viewsets.ModelViewSet):
                             status=status.HTTP_400_BAD_REQUEST)
         print(request.data)
         pass
+
 
 class VoteViewSet(viewsets.ModelViewSet):
     queryset = Vote.objects.all()
@@ -41,12 +46,14 @@ class VoteViewSet(viewsets.ModelViewSet):
 class ResultDoesNotExist(Exception):
     pass
 
+
 class PollViewSet(viewsets.ModelViewSet):
     queryset = Poll.objects.all()
     serializer_class = PollSerializer
 
     def get_result(self, abgeordnetenwatch_id):
-        r = requests.get(f'https://www.abgeordnetenwatch.de/api/v2/votes?poll={abgeordnetenwatch_id}&range_end=999')
+        r = requests.get(
+            f'https://www.abgeordnetenwatch.de/api/v2/votes?poll={abgeordnetenwatch_id}&range_end=999')
         votes = r.json()['data']
 
         result = {
@@ -58,7 +65,7 @@ class PollViewSet(viewsets.ModelViewSet):
 
         for vote in votes:
             result[vote['vote']] += 1
-        
+
         return result
 
     def list(self, request):
@@ -71,7 +78,8 @@ class PollViewSet(viewsets.ModelViewSet):
         if page_size == None:
             page_size = settings.REST_FRAMEWORK['PAGE_SIZE']
 
-        r = requests.get(f'https://www.abgeordnetenwatch.de/api/v2/polls?page={page_num}&pager_limit={page_size}')
+        r = requests.get(
+            f'https://www.abgeordnetenwatch.de/api/v2/polls?page={page_num}&pager_limit={page_size}')
         response = r.json()['data']
         poll_ids = []
         for poll in response:
@@ -82,30 +90,31 @@ class PollViewSet(viewsets.ModelViewSet):
 
                 if saved_poll.result == None:
                     raise ResultDoesNotExist
-                
-                print(f"Poll {saved_poll} found")
-                serializer = PollSerializer(saved_poll, context={'request': request})
 
+                print(f"Poll {saved_poll} found")
+                serializer = PollSerializer(
+                    saved_poll, context={'request': request})
 
             except ObjectDoesNotExist:
                 print("Poll not found, creating...")
-                
+
                 result = self.get_result(poll['id'])
 
                 new_poll = {
-                'title': poll['label'],
-                'abstract': poll['field_intro'],
-                'topics': poll['field_topics'],
-                'yes': result['yes'],
-                'no': result['no'],
-                'abstain': result['abstain'],
-                'no_show': result['no_show'],
-                'result': max(result, key=result.get),
-                'abgeordnetenwatch_id': poll['id'],
-                'abgeordnetenwatch_url': poll['api_url'],                    
+                    'title': poll['label'],
+                    'abstract': poll['field_intro'],
+                    'topics': poll['field_topics'],
+                    'yes': result['yes'],
+                    'no': result['no'],
+                    'abstain': result['abstain'],
+                    'no_show': result['no_show'],
+                    'result': max(result, key=result.get),
+                    'abgeordnetenwatch_id': poll['id'],
+                    'abgeordnetenwatch_url': poll['api_url'],
                 }
 
-                serializer = PollSerializer(data=new_poll, context={'request': request})
+                serializer = PollSerializer(
+                    data=new_poll, context={'request': request})
                 if serializer.is_valid():
                     serializer.save()
                     print('\nSerialized data single poll:')
@@ -113,11 +122,12 @@ class PollViewSet(viewsets.ModelViewSet):
                 else:
                     print('\nSerializer errors:')
                     print(serializer.errors)
-                
+                    return Response(serializer.errors,
+                                    status=status.HTTP_400_BAD_REQUEST)
+
             except ResultDoesNotExist:
                 print('Result does not exist')
                 pass
-            
 
         queryset = Poll.objects.filter(abgeordnetenwatch_id__in=poll_ids)
 
@@ -126,9 +136,7 @@ class PollViewSet(viewsets.ModelViewSet):
             serializer = self.get_serializer(page, many=True)
             return self.get_paginated_response(serializer.data)
 
-        serializer = PollSerializer(queryset, many=True, context={'request': request})
+        serializer = PollSerializer(
+            queryset, many=True, context={'request': request})
 
         return Response(serializer.data, status=status.HTTP_200_OK)
-            
-
-        
